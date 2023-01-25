9607c452b09a1b7881497b5cd9f3771dfd601b68
Erez <erezmirav@gmail.com>
2023-01-25 20:15:40 +0200

    updated about & contact page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Box } from '@mui/system';
+import Title from '../components/Title';
+import { Typography } from '@mui/material';
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <Box
+      sx={{
+        mt: -1,
+        textAlign: 'center',
+        justifyContent: 'center',
+        background: 'black',
+        padding: 2,
+        alignItems: 'center',
+      }}
+    >
+      <Typography
+        sx={{
+          fontFamily: 'Rubik',
+          fontWeight: 'bold',
+          color: 'white',
+          fontSize: 20,
+          mb: 2,
+        }}
+      >
+        About
+      </Typography>
+      <Typography sx={{ fontFamily: 'Rubik', color: 'white' }}>
+        This project was made for practicing ECommerce shop Frontend
+        development.
+        <br />
+        Entire Fronted was created using Material-UI Components.
+        <br />
+        Created by Erez Amirav.
+      </Typography>
+    </Box>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,110 @@
-import React from 'react'
+import React from 'react';
+import { Box } from '@mui/system';
+import { Button, Avatar, Grid, Typography } from '@mui/material';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ProfileImg from '../assets/img/meImg.jpg';
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <Box
+        sx={{
+          mt:-1,
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+          background: 'black',
+          padding: 2,
+          alignItems: 'center',
+        }}
+      >
+        <Typography
+          sx={{
+            fontFamily: 'Rubik',
+            fontWeight: 'bold',
+            color: 'white',
+            fontSize: 20,
+          }}
+        >
+          Contact Me
+        </Typography>
+        <Box
+          sx={{
+            textAlign: 'left',
+            justifyContent: 'center',
+            display: 'flex',
+            borderRadius: 5,
+            padding: 2,
+            alignItems: 'center',
+          }}
+        >
+          <Avatar
+            alt="Erez Amirav"
+            sx={{
+              width: '100px',
+              height: '100px',
+              border: '4px solid',
+              borderColor: 'rgb(255, 255, 255, 0.2)',
+            }}
+            src={ProfileImg}
+          />
+          <Grid>
+            <Grid item>
+              <Button
+                variant="filled"
+                href={'tel:+972509324402'}
+                sx={{
+                  color: 'white',
+                  fontFamily: 'Rubik',
+                  ':hover': {
+                    color: 'lime',
+                  },
+                }}
+              >
+                <LocalPhoneIcon fontSize="small" sx={{ mr: 1 }} />
+                +972 50-932-4402
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="filled"
+                sx={{
+                  color: 'white',
+                  fontFamily: 'Rubik',
+                  ':hover': {
+                    color: 'lime',
+                  },
+                }}
+                href={'mailto:erezamirav@gmail.com'}
+              >
+                <EmailIcon fontSize="small" sx={{ mr: 1 }} />
+                erezamirav@gmail.com
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="filled"
+                sx={{
+                  color: 'white',
+                  fontFamily: 'Rubik',
+                  ':hover': {
+                    color: 'lime',
+                  },
+                }}
+                target="_blank"
+                href={'https://www.linkedin.com/in/erez-amirav/'}
+              >
+                <LinkedInIcon fontSize="small" sx={{ mr: 1 }} />
+                Erez Amirav
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
