f30a9f6c8266f1918728de9d75cc713fb27911a2
Erez <erezmirav@gmail.com>
2023-01-27 12:52:03 +0200

    updated notfound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 function NotFound() {
   return (
-    <div>Error 404: Page Not Found</div>
-  )
+    <>
+      <Box
+        sx={{
+          mt: -1,
+          textAlign: 'center',
+          justifyContent: 'center',
+          background: 'black',
+          padding: 2,
+          alignItems: 'center',
+        }}
+      >
+        <Typography sx={{ fontFamily: 'Rubik', color: 'white', fontSize: 60 }}>
+          Error 404: <br />
+          Page Not Found
+        </Typography>
+        <Typography sx={{ fontFamily: 'Rubik', color: 'white', fontSize: 100 }}>
+          You Lost?
+        </Typography>
+        <Link to="/">
+          <Button
+            variant="contained"
+            sx={{
+              background: 'black',
+              fontFamily: 'Rubik',
+              mt: 6,
+              border: 2,
+              borderColor: 'white',
+              fontSize: 30,
+              fontWeight: 'bold',
+              ':hover': {
+                color: 'lime',
+                background: 'black',
+                borderColor: 'lime',
+              },
+            }}
+          >
+            Back to safety
+          </Button>
+        </Link>
+      </Box>
+    </>
+  );
 }
 
-export default NotFound
+export default NotFound;
